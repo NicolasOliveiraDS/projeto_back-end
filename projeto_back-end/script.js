d67f04053a9de1fd52edb67cd1294d24a05d28a7
Nicolas <nicolasfroyddrock@gmail.com>
2024-11-12 16:49:41 -0300

    first commit

---
+++ projeto_back-end/script.js
@@ -0,0 +1,120 @@
+window.revelar = ScrollReveal({reset:true});
+
+revelar.reveal('.nave',{
+    duration:3500,
+    origin:'bottom',
+    distance:'0px',
+    delay: 100,
+    interval: 300,
+    easing: 'ease'
+})
+
+revelar.reveal('.fecha',{
+    origin:'top',
+    duration:5000,
+    distance:'10px',
+    delay: 200,
+    interval: 400,
+    easing: 'ease'
+})
+revelar.reveal('.tabela',{
+    origin:'top',
+    duration:5000,
+    distance:'10px',
+    delay: 200,
+    interval: 400,
+    easing: 'ease'
+})
+
+revelar.reveal('.button-top',{
+    origin:'top',
+    duration:5000,
+    distance:'0px',
+    delay: 200,
+    interval: 400,
+    easing: 'ease'
+})
+
+revelar.reveal('.video',{
+    origin:'top',
+    duration:5000,
+    distance:'10px',
+    delay: 400,
+    interval: 100,
+    easing: 'ease'
+})
+
+revelar.reveal('.legend',{
+    origin:'top',
+    duration:5000,
+    distance:'10px',
+    delay: 400,
+    interval: 300,
+    easing: 'ease'
+})
+
+revelar.reveal('.vj',{
+    duration:2000,
+    distance:'10px',
+    delay: 100,
+    interval: 200,
+    easing: 'ease'
+})
+
+revelar.reveal('.center-popup',{
+    duration:2000,
+    distance:'90px',
+    delay: 100,
+    interval: 200,
+    easing: 'ease'
+})
+revelar.reveal('.login',{
+    duration:2000,
+    distance:'90px',
+    delay: 100,
+    interval: 200,
+    easing: 'ease'
+})
+
+revelar.reveal('.goria',{
+    origin: 'top',
+    duration:5000,
+    distance:'10px',
+    delay: 400,
+    interval: 300,
+    easing: 'ease',
+    
+})
+
+revelar.reveal('.card0',{
+    origin: 'top',
+    duration:5000,
+    distance:'10px',
+    delay: 400,
+    interval: 300,
+    easing: 'ease',
+})
+revelar.reveal('.card1',{
+    origin: 'top',
+    duration:5000,
+    distance:'10px',
+    delay: 400,
+    interval: 300,
+    easing: 'ease',
+})
+revelar.reveal('.card2',{
+    origin: 'top',
+    duration:5000,
+    distance:'10px',
+    delay: 400,
+    interval: 300,
+    easing: 'ease',
+})
+revelar.reveal('.card3',{
+    origin: 'top',
+    duration:5000,
+    distance:'10px',
+    delay: 400,
+    interval: 300,
+    easing: 'ease',
+})
