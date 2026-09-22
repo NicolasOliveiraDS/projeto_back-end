7b44bf4868ca1e8390e1bd4a04c6f9b5fae49c96
nicola oliveira <nicolasfroyddrock@gmail.com>
2026-09-22 19:05:09 -0300

    Adiciona interacao do botao jogar

--- projeto_back-end/js/app.js
+++ projeto_back-end/js/app.js
@@ -1,1 +1,14 @@
-console.log("Battle Arena iniciado!");
+const botaoJogar = document.querySelector("#btn-jogar");
+const lobby = document.querySelector(".lobby");
+
+botaoJogar.addEventListener("click", function () {
+  lobby.innerHTML = `
+    <p>PARTIDA ENCONTRADA</p>
+
+    <h2>Entrando na arena...</h2>
+
+    <p>
+      Prepare-se. A batalha vai começar.
+    </p>
+  `;
+});
